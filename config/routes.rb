8fe1c2e5f15fbfffa729c8a2b8ecbd4c803de4f7
github username <github email address>
2020-02-04 16:01:44 -0500

    Yup Yup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :ducks, only: [:index]
-  resources :students, only: [:index]
+  resources :ducks, only: [:index, :new, :create, :edit, :update, :show]
+  resources :students, only: [:index, :new, :create, :edit, :update, :show]
 end
